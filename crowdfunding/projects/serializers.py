83bc5bc543b00b71ad9c124481907b02d41bee3e
Simplicity305 <113986047+Simplicity305@users.noreply.github.com>
2023-01-21 00:28:04 +0800

    Setup & model setup completed

--- crowdfunding/projects/serializers.py
+++ crowdfunding/projects/serializers.py
@@ -3,6 +3,13 @@ from rest_framework import serializers
 from .models import Project, Pledge
 
 
+class PledgeSerializer(serializers.ModelSerializer): #used model forms which interprets the model and this works similarly 
+    class Meta:
+        model = Pledge
+        fields = ['id', 'amount', 'comment', 'anonymous', 'project', 'supporter']
+        #fields = '__all__' - could have also done it this way instead of listing them all out 
+
+
 class ProjectSerializer(serializers.Serializer):
     id = serializers.ReadOnlyField() 
     title = serializers.CharField(max_length=200)
@@ -11,16 +18,18 @@ class ProjectSerializer(serializers.Serializer):
     image = serializers.URLField()
     is_open = serializers.BooleanField()
     date_created = serializers.DateTimeField()
-    owner = serializers.CharField(max_length=200)
+    # owner = serializers.CharField(max_length=200) #NOTE check this - appears in first setup slide, but not the models handling one
+    # pledges = PledgeSerializer(many=True, read_only=True) NOTE Per Thinkific slides, this line was struck through and the be
+
     
     #Need to add this because otherwise we get an error that says that create isnt a thing in thr webpage without this. Serialiser doesnt know what we want to do with it even though we told it to save in views (I think)
     def create(self, validated_data):
         return Project.objects.create(**validated_data) #validated data is a dictionary that represents the data from our serialiser. ** says take everything in here and return in key values pair eg title: name of title 
     
-class PledgeSerializer(serializers.ModelSerializer): #used model forms which interprets the model and this works similarly 
-    class Meta:
-        model = Pledge
-        fields = ['id', 'amount', 'comment', 'anonymous', 'project', 'supporter']
-        #fields = '__all__' - could have also done it this way instead of listing them all out 
+# SPLITTING THE PROJECT SERIALIZER INTO TWO
+# Previously when it was just the Project serializer class, the pledges would appear under each project. By creating a new class and changing the project detail class in Views to refer to this serializer instead, we remove the pledges from showing on the projects page
+class ProjectDetailSerializer(ProjectSerializer):
+	pledges = PledgeSerializer(many=True, read_only=True)
+
         
         
--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render
 from django.http import Http404
-# Create your views here.
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, generics
 
 from .models import Project, Pledge
-from .serializers import ProjectSerializer, PledgeSerializer
+from .serializers import ProjectSerializer, PledgeSerializer, ProjectDetailSerializer
 
 class ProjectList(APIView):
+    
     def get(self, request):
-        projects = Project.objects.all() #getting all the projects from projects
+        projects = Project.objects.all() 
+        #getting all the projects from projects
         serializer = ProjectSerializer(projects, many=True) #serialsiers arent smart enough so you have to tell it - hence the many =true
         return Response(serializer.data) #because we're getting the data out of the serializer so we have to ask it for the data 
+    
     
     def post(self, request): #request is going to be the request object that django receives. it receives a request object and its going to do stuff with it - we're getting the payload from the request gets sent. because of the way that the api view works, it will always give you request and if you dont handle it, it wont work.abs(x)
         serializer = ProjectSerializer(data=request.data)
@@ -24,6 +27,7 @@ class ProjectList(APIView):
         #this added a thing in the projects page - media type and content. we want to give it some 
     
 class ProjectDetail(APIView):
+    
     def get_object(self, pk): 
         try: 
             return Project.objects.get(pk=pk) #means that pk is equal to the primary key that its been given - use pk because its not always id - could be username but has to match what we put in our url 
@@ -34,12 +38,13 @@ class ProjectDetail(APIView):
     
     def get(self, request, pk):
         project = self.get_object(pk)
-        serializer = ProjectSerializer(project) #means serialise this thing in the brackets?
+        serializer = ProjectDetailSerializer(project) #means serialise this thing in the brackets?
         return Response(serializer.data)
     #going to create an instance of a project serialiser and 
 
     #hes looking for a list create view in api classydrf 
     #THIS PART CONNECTS TO THE SERIALISER PLEDGE 
+
     
 class PledgeList(generics.ListCreateAPIView):
     queryset = Pledge.objects.all()
